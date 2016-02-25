4fa72caa47737e7ab89bb95384f868a4e450dd84
gerak1925 <ikarous1925@gmail.com>
2016-02-25 12:51:54 +0200

    added solution for 04_pig_latin

---
+++ 04_pig_latin/pig_latin.rb
@@ -0,0 +1,18 @@
+def translate (string)
+  	words = string.split(" ")
+  	words.each do |word|
+    temp = ""
+    until ['a','e','i','o','u'].include?(word[0]) do
+      if (0 < word.length-1 && word[0]=='q' && word[1]=='u')
+        temp << word[0,2]
+        word[0,2]= ''
+      else
+        temp << word[0]
+        word[0] = ''
+      end
+    end
+    word << temp + "ay"
+  end
+
+  words.join(" ")
+end
